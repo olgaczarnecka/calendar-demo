f1f48454337f7b1d50696ff34860a98a0489d365
Olga Czarnecka <olga.czarnecka@lunarlogic.io>
2019-12-16 16:25:23 +0100

    Add basic calendar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Calendar, CalendarControls } from "react-yearly-calendar";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const customCSSclasses = {
+    holidays: [
+      "2018-04-25",
+      "2018-05-01",
+      "2018-06-02",
+      "2018-08-15",
+      "2018-11-01"
+    ],
+    spring: {
+      start: "2018-03-21",
+      end: "2018-6-20"
+    },
+    summer: {
+      start: "2018-06-21",
+      end: "2018-09-22"
+    },
+    autumn: {
+      start: "2018-09-23",
+      end: "2018-12-21"
+    },
+    weekend: "Sat,Sun"
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CalendarControls year={2018} />
+      <Calendar
+        year={2018}
+        customClasses={customCSSclasses}
+        firstDayOfWeek={1}
+      />
     </div>
   );
 }
